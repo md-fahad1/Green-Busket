e2e606dc448b72027e36f09eda78b5e92a6e9540
farseit <farseit4@gmail.com>
2025-07-19 16:45:10 +0600

    spelling

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -10,7 +10,7 @@ import NewArrivalContainer from "@/components/Home/HomePage/NewArrival/NewArriva
 import StylishShop from "@/components/Home/HomePage/StylishShop";
 import TopBrandProducts from "@/components/Home/HomePage/TopBrandProducts/TopBrandProducts";
 import TopCategories from "@/components/Home/HomePage/TopCategory/Topcategory";
-import Vegetable from "@/components/Home/HomePage/Vegetable/vegetable";
+import Vegetable from "@/components/Home/HomePage/Vegetable/Vegetable";
 import ProductContainer from "@/components/Product/ProductContainer";
 import WhatsApp from "@/components/Whatsapp/WhatsApp";
 import { Inter } from "next/font/google";
